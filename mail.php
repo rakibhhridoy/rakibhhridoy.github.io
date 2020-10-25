75c8835497079dae5c09c6f32010176e79916adf
rakibhhridoy <rakibhhridoy05@gmail.com>
2020-10-25 22:19:13 +0600

    navbar fixed for mobile users

--- mail.php
+++ mail.php
@@ -17,24 +17,26 @@ if(asset($_POST['submit']){
 	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'user@example.com';                 // SMTP username
-	$mail->Password = 'secret';                           // SMTP password
+	$mail->Username = 'mailrakibhhridoy@gmail.com';                 // SMTP username
+	$mail->Password = 'Set';                           // SMTP password
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
 	$mail->setFrom($email, $name);
-	$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient      	
+	$mail->addAddress('mailrakibhhridoy@gmail.com');     // Add a recipient      	
 	$mail->Subject = ('$email ($form_subject)')
-	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->Body    = $msg
 
 	if(!$mail->send()) {
-	echo 'Message could not be sent.';
-	echo 'Mailer Error: ' . $mail->ErrorInfo;
+		$status = 'Message could not be sent.';
+		$response =  'Mail Error: ' . $mail->ErrorInfo;
 	} else {
-	echo 'Message has been sent';
+		$status =  'Message has been sent';
 	}
+
+	exit(json_encode(array('status'=> $status, 'response' => $response)));
 
 
 
