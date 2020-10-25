d1d38d952d100bef78529096cfd3ab1360dd681c
rakibhhridoy <rakibhhridoy05@gmail.com>
2020-10-25 21:17:53 +0600

    heading color changed of .heading in style.css

--- mail.php
+++ mail.php
@@ -1,18 +1,48 @@
 <?php
-	if(asset($_POST['submit']){
-		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$phone=$_POST['phone'];
-        $msg=$_POST['msg'];
-        $form_subject=$_POST['subject']
 
-	$to='rakibhhridoy@yahoo.com';
-	$subject=$form_subject
-	$message='Name:'.$name.'\n'.'Phone: '.$phone.'\n'.'Wrote the following: '.'\n'.$msg;
-	$headers='From: '.$email;
-	
-	if(mail($to,$subject,$message, $headers)){
-		echo '<h1>Success</h1>';
-		}
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(asset($_POST['submit']){
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$msg=$_POST['msg'];
+	$form_subject=$_POST['subject'];
+
+	require_once 'PHPMailer/src/PHPMailer.php';
+	require_once 'PHPMailer/src/SMTP.php';
+	require_once 'PHPMailer/src/Exception.php';
+
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'user@example.com';                 // SMTP username
+	$mail->Password = 'secret';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
+
+	$mail->setFrom($email, $name);
+	$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient      	
+	$mail->Subject = ('$email ($form_subject)')
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+	if(!$mail->send()) {
+	echo 'Message could not be sent.';
+	echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+	echo 'Message has been sent';
 	}
+
+
+
+
+
+
+
+}
+
+
 ?>
